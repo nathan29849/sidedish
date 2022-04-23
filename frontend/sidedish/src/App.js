229d6ca6327c313bf83b4e90e17ee866a00bbbe1
onlinekkabchi <onlinekkabchi@gmail.com>
2022-04-23 20:31:01 +0900

    test

--- frontend/sidedish/src/App.js
+++ frontend/sidedish/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import BigSidedish from "./BigSidedish";
 import SmallSidedish from "./SmallSidedish";
+import Popup from "./Popup";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Header />
             <BigSidedish />
             <SmallSidedish />
+            <Popup />
         </>
     );
 }
