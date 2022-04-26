01764e16f878070bc3cb328d4e517069685a3118
onlinekkabchi <onlinekkabchi@gmail.com>
2022-04-26 15:29:44 +0900

    test: [#28] slider refactoring ver.3

--- frontend/sidedish/src/view/sidedish/SmallSidedish.js
+++ frontend/sidedish/src/view/sidedish/SmallSidedish.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState, useRef } from "react";
 import { Animated, FlatList, View } from "react-native-web";
 import "./SmallSidedish.css";
+import "./Carousel.css";
 
 function SmallCard({ id, img, text, des, pre, cur, lan }) {
     return (
@@ -28,57 +29,41 @@ function SmallCard({ id, img, text, des, pre, cur, lan }) {
     );
 }
 
-function SmallSidedishCard() {
-    const [error, setError] = useState("null");
-    const [isLoaded, setIsLoaded] = useState("false");
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        fetch(
-            "https://273b4433-0674-40c4-9d88-6ab939cd01f8.mock.pstmn.io/api/dish?section=정갈한-밑반찬"
-        )
-            .then((res) => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                    setItems(result);
-                    setIsLoaded(true);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            );
-    }, []);
-
-    if (isLoaded === true) {
-        return (
-            <>
-                {items.data.map((dish) => {
-                    return (
-                        <>
-                            <SmallCard
-                                id={dish.dishId}
-                                img={dish.imagePath}
-                                text={dish.title}
-                                des={dish.description}
-                                pre={dish.price}
-                                cur={dish.price}
-                                lan={dish.stock}
-                            />
-                        </>
-                    );
-                })}
-            </>
-        );
-    }
-}
-
 function CarouselItem({ children, width }) {
     return (
         <div className="carousel-item" style={{ width: width }}>
             {children}
         </div>
+    );
+}
+
+function SampleCard() {
+    return (
+        <li className="sample-sidedish__card" id="1">
+            <div className="sample-sidedish__card-img-container">
+                <img
+                    className="sample-sidedish__card-img"
+                    src="https://i.ytimg.com/vi/y5X-5L2VJk0/maxresdefault.jpg"
+                />
+            </div>
+            <div className="sample-sidedish__card-item">
+                <div className="sample-sidedish__card-item--text">
+                    <h3 className="sample-sidedish__card-name">잡채</h3>
+                    <p className="sample-sidedish__card-description">
+                        맛있는잡채
+                    </p>
+                </div>
+            </div>
+            <div className="sample-sidedish__card-prices">
+                <span className="sample-sidedish__card-price--default">
+                    1000
+                </span>
+                <span className="sample-sidedish__card-price--option">
+                    1000
+                </span>
+            </div>
+            <span className="sample-sidedish__card-tag--launch">특별세일</span>
+        </li>
     );
 }
 
@@ -94,14 +79,16 @@ function Carousel({ children }) {
         setActiveIndex(newIndex);
     };
     return (
-        <div className="carousel">
-            <div
-                className="inner"
-                style={{ transform: `translateX(-${activeIndex * 100}%)` }}
-            >
-                {React.Children.map(children, (child, index) => {
-                    return React.cloneElement(child, { width: "100%" });
-                })}
+        <>
+            <div className="carousel">
+                <div
+                    className="inner"
+                    style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+                >
+                    {React.Children.map(children, (child, index) => {
+                        return React.cloneElement(child, { width: "100%" });
+                    })}
+                </div>
             </div>
             <div className="indicators">
                 <button
@@ -119,7 +106,7 @@ function Carousel({ children }) {
                     Next
                 </button>
             </div>
-        </div>
+        </>
     );
 }
 
@@ -146,28 +133,37 @@ function SmallSidedish() {
             );
     }, []);
 
-    return (
-        <div className="small-sidedish">
-            <div className="small-sidedish__header">
-                <h3 className="small-sidedish__title">
-                    식탁을 풍성하게 하는 정갈한 밑반찬
-                </h3>
+    const per = "100%";
+    if (isLoaded === true) {
+        return (
+            <div className="small-sidedish">
+                <div className="small-sidedish__header">
+                    <h3 className="small-sidedish__title">
+                        식탁을 풍성하게 하는 정갈한 밑반찬
+                    </h3>
+                </div>
+
+                <Carousel>
+                    {items.data.map((dish) => {
+                        return (
+                            <>
+                                <CarouselItem width={per}>
+                                    <SampleCard
+                                        img={dish.imagePath}
+                                        text={dish.title}
+                                        des={dish.description}
+                                        pre={dish.price}
+                                        cur={dish.price}
+                                        lan={dish.stock}
+                                    ></SampleCard>
+                                </CarouselItem>
+                            </>
+                        );
+                    })}
+                </Carousel>
             </div>
-
-            <Carousel></Carousel>
-        </div>
-    );
+        );
+    }
 }
 
-{
-    /* <SmallCard
-        id={dish.dishId}
-        img={dish.imagePath}
-        text={dish.title}
-        des={dish.description}
-        pre={dish.price}
-        cur={dish.price}
-        lan={dish.stock}
-    />; */
-}
 export default SmallSidedish;
