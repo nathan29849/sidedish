54d96c07f474b8c3022145da2dc6ad655f6bdfc7
onlinekkabchi <onlinekkabchi@gmail.com>
2022-04-28 13:53:33 +0900

    style: [#47] product detail popup styling 진행중 2

--- frontend/sidedish/src/view/popup/Popup.jsx
+++ frontend/sidedish/src/view/popup/Popup.jsx
@@ -7,9 +7,14 @@ import {
     PopupProductImgMini,
     PopupProductDetail,
     PopupProductInfo,
+    PopupProductInfoCloseBtn,    
     PopupProductDetailSection1,
     PopupProductDetailSection2,
     PopupProductDetailSection3,
+    PopupProductInfoName,
+    PopupProductInfoPrimeCost,
+    PopupProductInfoBadgeAndPrice,
+    PopupProductInfoBadgeAndPriceTag,    
     PopupproductInfoPoint,
     PopupproductInfoPointText,
     PopupproductInfoPointLabel,
@@ -19,11 +24,24 @@ import {
     PopupproductInfoCharge,
     PopupproductInfoChargeText,
     PopupproductInfoChargeLabel,
-    PopupProductInfoCloseBtn,
-    PopupProductInfoName,
-    PopupProductInfoPrimeCost,
-    PopupProductInfoBadgeAndPrice,
-    PopupProductInfoBadgeAndPriceTag,
+    PopupProductOrderTotalAmount,
+    PopupProductOrderTotalNumber,
+    PopupProductOrderNumberPlusBtn,
+    PopupProductOrderNumberMinusBtn,
+    PopupProductOrderTotalCost,
+    PopupProductOrderTotalCostText,
+    PopupProductOrderTotalCostLabel,
+    PopupProductOrderChoiceBtn,
+    PopupProductOrderChoiceBtnText,
+    RelatedProduct,
+    RelatedProductTitle,
+    RelatedProductNav,
+    RelatedProductNavWholePage,
+    RelatedProductNavCurrentPage,
+    RelatedProductNavRightBtn,
+    RelatedProductNavLeftBtn,
+    RelatedProductWrapScroll,
+    RelatedProductCard
 } from "./Popup.style";
 
 function SmallCard({ id, img, text, des, pre, cur, lan }) {
@@ -106,10 +124,9 @@ function ProductDetailPopup() {
                 <PopupProductImg src="https://recipe1.ezmember.co.kr/cache/recipe/2020/08/30/55158f507b8a58f229f6b20d587bd2b91.jpg" />
                 <PopupProductImgMini src="https://recipe1.ezmember.co.kr/cache/recipe/2020/08/30/55158f507b8a58f229f6b20d587bd2b91.jpg" />
             </PopupProductImgContainer>
-            <div className="popup__product-detail">
-                
                 <PopupProductInfo>
                     <PopupProductInfoCloseBtn>닫기</PopupProductInfoCloseBtn>
+                    
                     <PopupProductDetailSection1>
                         <PopupProductInfoName>오리_주물럭_반조리</PopupProductInfoName>
                         <PopupProductInfoPrimeCost>15000원</PopupProductInfoPrimeCost>
@@ -131,98 +148,34 @@ function ProductDetailPopup() {
                         </PopupproductInfoCharge>
                     </PopupProductDetailSection2>
                     <PopupProductDetailSection3>
-
-
+                        <PopupProductOrderTotalAmount>
+                            <PopupProductOrderNumberMinusBtn>minus</PopupProductOrderNumberMinusBtn>
+                            <PopupProductOrderTotalNumber>1</PopupProductOrderTotalNumber>
+                            <PopupProductOrderNumberPlusBtn>plus</PopupProductOrderNumberPlusBtn>
+                        </PopupProductOrderTotalAmount>
+                        <PopupProductOrderTotalCost>
+                            <PopupProductOrderTotalCostLabel>총 주문금액</PopupProductOrderTotalCostLabel>
+                            <PopupProductOrderTotalCostText>12640원</PopupProductOrderTotalCostText>
+                        </PopupProductOrderTotalCost>
                     </PopupProductDetailSection3>
-
+                    <PopupProductOrderChoiceBtn>
+                        <PopupProductOrderChoiceBtnText>주문하기</PopupProductOrderChoiceBtnText>
+                    </PopupProductOrderChoiceBtn>
                 </PopupProductInfo>
-                
-                {/* <div className="popup__product-detail--section1">
-                    <h3 className="popup__product-title">
-                        오리 주물럭_반조리
-                    </h3>
-                    <span className="popup__product-price--default">
-                        15000원
-                    </span>
-                    <span className="popup__product-tag--launch">
-                        런칭특가
-                    </span>
-                    <span className="popup__product-price--option">
-                        12640원
-                    </span>
-                </div> */}
-                {/* <div className="popup__product-detail--section2">
-                    <div className="popup__product-point">
-                        <h2 className="popup__product-point--name">
-                            적립금
-                        </h2>
-                        <span className="popup__product-point--price">
-                            1200
-                        </span>
-                    </div>
-                    <div className="popup__product-delivery-info">
-                        <h2 className="popup__product-delivery-info--name">
-                            배송정보
-                        </h2>
-                        <span className="popup__product-delivery-info--area">
-                            서울 경기 새벽배송, 전국 택배배송
-                        </span>
-                    </div>
-                    <div className="popup__product-delivery-price">
-                        <h2 className="popup__product-delivery-price--name">
-                            배송비
-                        </h2>
-                        <span className="popup__product-delivery-price--payment">
-                            2500원[40000원 이상 구매 시 무료]
-                        </span>
-                    </div>
-                </div> */}
-                <div className="popup__product-detail--section3">
-                    <div className="popup__product-order-number">
-                        <button className="popup__product-order-number-btn--remove">
-                            -
-                        </button>
-                        <span className="popup__product-order-number-result"></span>
-                        <button className="popup__product-order-number-btn--add">
-                            +
-                        </button>
-                    </div>
-                    <div className="popup__product-order-result">
-                        <span className="popup__product-order-result--name">
-                            총주문금액
-                        </span>
-                        <span className="popup__product-order-result--price">
-                            12640원
-                        </span>
-                    </div>
-                    <button className="popup__product-order-choice-btn">
-                        주문하기
-                    </button>
-                </div>
-            </div>
         </PopupProduct>
-        
-        <div className="popup__related-product">
-            <div className="popup__related-product__header">
-                <span className="popup__related-product__header-title">
-                    함꼐하면 더욱 맛있는 상품
-                </span>
-                <span className="popup__related-product__header-slider">
-                    <button className="popup__related-product__header-slider__btn--back">
-                        -
-                    </button>
-                    <button className="popup__related-product__header-slider__info">
-                        1
-                    </button>
-                    <button className="popup__related-product__header-slider__btn--on">
-                        +
-                    </button>
-                </span>
-            </div>
-            <div className="popup__related-product__cards small-sidedish__cards">
-                <SmallSidedishCard />
-            </div>
-        </div>
+        <RelatedProduct>
+            <RelatedProductTitle>함꼐하면 더욱 맛있는 상품</RelatedProductTitle>
+            <RelatedProductNav>
+                <RelatedProductNavLeftBtn></RelatedProductNavLeftBtn>
+                <RelatedProductNavWholePage>4</RelatedProductNavWholePage>
+                <RelatedProductNavCurrentPage>1</RelatedProductNavCurrentPage>
+                <RelatedProductNavRightBtn></RelatedProductNavRightBtn>
+            </RelatedProductNav>
+            <RelatedProductWrapScroll>
+                <RelatedProductCard />
+
+            </RelatedProductWrapScroll>
+        </RelatedProduct>
     </Popup>
     </>
     );
--- frontend/sidedish/src/view/popup/Popup.style.js
+++ frontend/sidedish/src/view/popup/Popup.style.js
@@ -31,12 +31,12 @@ const PopupProductImgMini = styled.img`
     left: 128px;
     top: 476px;
 `;
-
 const PopupProductDetail = styled.button`
     width: auto;
 `;
-
 const PopupProductInfo = styled.div`
+    display: flex;
+    flex-direction: column;
     width: 440px;
     height: 237px;
     left: 472px;
@@ -370,6 +370,325 @@ const PopupproductInfoChargeLabel = styled.span`
     flex-grow: 0;
     margin: 0px 16px;
 `;
+const PopupProductOrderTotalAmount = styled.span`
+    /* Totalamount */
+
+    /* Auto layout */
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+
+    width: 88px;
+    height: 26px;
+    left: 480px;
+    top: 339px;
+`;
+const PopupProductOrderTotalNumber = styled.span`
+    /* Number */
+
+    position: static;
+    width: 40px;
+    height: 26px;
+    left: 24px;
+    top: 0px;
+
+    /* Bold/Medium Bold */
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 26px;
+    /* identical to box height, or 162% */
+
+    text-align: center;
+    letter-spacing: -0.008em;
+
+    /* GreyScale/Black */
+
+    color: #1b1b1b;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+    margin: 0px 0px;
+`;
+const PopupProductOrderNumberPlusBtn = styled.button`
+    /* Vector */
+
+    left: 28.12%;
+    right: 28.12%;
+    top: 50%;
+    bottom: 50%;
+
+    /* GreyScale/Grey2 */
+
+    border: 2px solid #777777;
+`;
+const PopupProductOrderNumberMinusBtn = styled.button`
+    /* Vector */
+
+    left: 28.12%;
+    right: 28.12%;
+    top: 50%;
+    bottom: 50%;
+
+    /* GreyScale/Grey2 */
+
+    border: 2px solid #777777;
+`;
+const PopupProductOrderTotalCost = styled.div`
+    display: flex;
+`;
+const PopupProductOrderTotalCostText = styled.span`
+    /* 1 */
+
+    position: static;
+    width: 77px;
+    height: 26px;
+    left: 0px;
+    top: 2px;
+
+    /* Regular/Medium Regular */
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 26px;
+    /* identical to box height, or 162% */
+
+    text-align: right;
+    letter-spacing: -0.008em;
+
+    /* GreyScale/Grey2 */
+
+    color: #777777;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0px 16px;
+`;
+const PopupProductOrderTotalCostLabel = styled.span`
+    /* 2 */
+
+    position: static;
+    width: 81px;
+    height: 30px;
+    left: 93px;
+    top: 0px;
+
+    /* Bold/Large Bold */
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 30px;
+    /* identical to box height, or 150% */
+
+    text-align: right;
+    letter-spacing: -0.008em;
+
+    /* GreyScale/Black */
+
+    color: #1b1b1b;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+    margin: 0px 16px;
+`;
+const PopupProductOrderChoiceBtn = styled.button`
+    /* button */
+
+    /* Auto layout */
+
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 16px;
+
+    width: 440px;
+    height: 58px;
+    left: 472px;
+    top: 490px;
+
+    /* GreyScale/Black */
+
+    background-color: #1b1b1b;
+`;
+const PopupProductOrderChoiceBtnText = styled.span`
+    /* Text */
+
+    width: 408px;
+    height: 26px;
+    left: 16px;
+    top: 16px;
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 26px;
+    /* identical to box height */
+
+    text-align: center;
+
+    /* White */
+
+    color: #ffffff;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0px 10px;
+`;
+const RelatedProduct = styled.div`
+    /* RelatedProduct */
+
+    width: 960px;
+    height: 396px;
+    left: 0px;
+    top: 598px;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 2;
+    flex-grow: 0;
+    margin: 0px 0px;
+`;
+const RelatedProductTitle = styled.span`
+    /* Title */
+
+    position: absolute;
+    width: 214px;
+    height: 30px;
+    left: 48px;
+    top: 646px;
+
+    /* Bold/Large Bold */
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 30px;
+    /* identical to box height, or 150% */
+
+    letter-spacing: -0.008em;
+
+    /* GreyScale/Grey1 */
+
+    color: #3f3f3f;
+`;
+const RelatedProductNav = styled.div`
+    /* Nav */
+
+    /* Auto layout */
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+
+    position: absolute;
+    width: 102px;
+    height: 24px;
+    left: 810px;
+    top: 649px;
+`;
+const RelatedProductNavWholePage = styled.span`
+    width: 8px;
+    height: 24px;
+    left: 62px;
+    top: 0px;
+
+    /* Regular/Small Regular */
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    /* identical to box height, or 171% */
+
+    letter-spacing: -0.008em;
+
+    /* GreyScale/Grey1 */
+
+    color: #3f3f3f;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 3;
+    flex-grow: 0;
+    margin: 0px 8px;
+`;
+const RelatedProductNavCurrentPage = styled.span`
+    width: 8px;
+    height: 24px;
+    left: 62px;
+    top: 0px;
+
+    /* Regular/Small Regular */
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    /* identical to box height, or 171% */
+
+    letter-spacing: -0.008em;
+
+    /* GreyScale/Grey1 */
+
+    color: #3f3f3f;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 3;
+    flex-grow: 0;
+    margin: 0px 8px;
+`;
+const RelatedProductNavRightBtn = styled.span``;
+const RelatedProductNavLeftBtn = styled.span``;
+const RelatedProductWrapScroll = styled.div`
+    width: 864px;
+    height: 226px;
+    left: 48px;
+    top: 704px;
+`;
+const RelatedProductCard = styled.img`
+    width: 160px;
+    height: 160px;
+    left: 0px;
+    top: 0px;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 8px 0px;
+`;
 export {
     Popup,
     PopupProduct,
@@ -378,9 +697,14 @@ export {
     PopupProductImgMini,
     PopupProductDetail,
     PopupProductInfo,
+    PopupProductInfoCloseBtn,
     PopupProductDetailSection1,
     PopupProductDetailSection2,
     PopupProductDetailSection3,
+    PopupProductInfoName,
+    PopupProductInfoPrimeCost,
+    PopupProductInfoBadgeAndPrice,
+    PopupProductInfoBadgeAndPriceTag,
     PopupproductInfoPoint,
     PopupproductInfoPointText,
     PopupproductInfoPointLabel,
@@ -390,11 +714,24 @@ export {
     PopupproductInfoCharge,
     PopupproductInfoChargeText,
     PopupproductInfoChargeLabel,
-    PopupProductInfoCloseBtn,
-    PopupProductInfoName,
-    PopupProductInfoPrimeCost,
-    PopupProductInfoBadgeAndPrice,
-    PopupProductInfoBadgeAndPriceTag,
+    PopupProductOrderTotalAmount,
+    PopupProductOrderTotalNumber,
+    PopupProductOrderNumberPlusBtn,
+    PopupProductOrderNumberMinusBtn,
+    PopupProductOrderTotalCost,
+    PopupProductOrderTotalCostText,
+    PopupProductOrderTotalCostLabel,
+    PopupProductOrderChoiceBtn,
+    PopupProductOrderChoiceBtnText,
+    RelatedProduct,
+    RelatedProductTitle,
+    RelatedProductNav,
+    RelatedProductNavWholePage,
+    RelatedProductNavCurrentPage,
+    RelatedProductNavRightBtn,
+    RelatedProductNavLeftBtn,
+    RelatedProductWrapScroll,
+    RelatedProductCard,
 };
 
 // .popup{
